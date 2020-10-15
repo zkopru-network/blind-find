eea7c73d5f0dc34a04a1cd65902b62a0ad1c7f4e
mhchia <kevin.mh.chia@gmail.com>
2020-10-15 18:31:07 +0800

    config.ts: fix `q`
    `q` should be the subgroup order

--- src/smp/config.ts
+++ src/smp/config.ts
@@ -1,7 +1,7 @@
 import { babyJub } from 'circomlib';
-import { PubKey as ECPoint, SNARK_FIELD_SIZE } from 'maci-crypto';
+import { PubKey as ECPoint } from 'maci-crypto';
 
-const q = SNARK_FIELD_SIZE;
+const q = babyJub.subOrder as BigInt;
 const G = babyJub.Base8 as ECPoint;
 
 export { q, G, ECPoint };
