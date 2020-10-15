2e187c06da06c9b89df21ec0fea86c908373448c
mhchia <kevin.mh.chia@gmail.com>
2020-10-15 16:30:01 +0800

    Add `bigIntMod` and its tests

--- src/smp/utils.ts
+++ src/smp/utils.ts
@@ -1,9 +1,20 @@
+import BN from 'bn.js';
+
 /**
  * Concatenate two `Uint8Array` into one.
  */
-export function concatUint8Array(a: Uint8Array, b: Uint8Array): Uint8Array {
+export const concatUint8Array = (a: Uint8Array, b: Uint8Array): Uint8Array => {
   let c = new Uint8Array(a.length + b.length);
   c.set(a);
   c.set(b, a.length);
   return c;
+}
+
+/**
+ * Modular operation for `BigInt`.
+ * @param a Number to be reduced
+ * @param modulus Modulus
+ */
+export const bigIntMod = (a: BigInt, modulus: BigInt): BigInt => {
+  return BigInt(new BN(a.toString()).umod(new BN(modulus.toString())).toString());
 }
