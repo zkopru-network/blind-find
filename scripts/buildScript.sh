65602bb77d4df3829da16f2778c3599b4403383c
mhchia <kevin.mh.chia@gmail.com>
2020-11-28 17:22:11 +0800

    Try cache

--- scripts/buildScript.sh
+++ scripts/buildScript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Usage: compile circom code
+# Usage: compile circom code and perform trusted setup
 # Example: ./buildScript.sh xxx.circom
 
 dir=`dirname $0`
-build_dir="$dir/build"
+projectRootDir="$dir/.."
+build_dir="$projectRootDir/build"
 mkdir -p $build_dir
 circuit_filename=`basename $1`
 circuit_name="${circuit_filename%%.*}"
@@ -15,6 +16,6 @@ pr_path="$build_dir/${circuit_name}.params"
 
 # NODE_OPTIONS=--max-old-space-size=16384 ts-node $build_snark_script -i $circuit_path -j $r1cs_path -w $wasm_path -p $pk_path -v $vk_path -pr $pr_path -s $sol_out -vs $vs_sol_out
 # (sh buildScript.sh src/circuits/ts/t.circom) && cd src/circuits/ts/ &&  ts-node index.ts && cd -
-node $dir/node_modules/circom/cli.js $1 -r $r1cs_path -w $wasm_path
+node $projectRootDir/node_modules/circom/cli.js $1 -r $r1cs_path -w $wasm_path
 zkutil setup -c $r1cs_path -p $pr_path
 zkutil export-keys -c $r1cs_path -p $pr_path -r $pk_path -v $vk_path
---
+++ scripts/installZkutil.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+which cargo > /dev/null
+if [ "$?" != "0" ]
+then
+    # Install rust and cargo
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+
+which zkutil > /dev/null
+if [ "$?" != "0" ]
+then
+    # Install zkutil
+    cargo install zkutil --version 0.3.2 && zkutil --help
+fi
