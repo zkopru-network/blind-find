2a6a868148c8abe935265e1dca6a8e31f0bbf8d7
mhchia <kevin.mh.chia@gmail.com>
2020-11-30 00:04:55 +0800

    2

--- scripts/installZkutil.sh
+++ scripts/installZkutil.sh
@@ -3,13 +3,13 @@
 which cargo > /dev/null
 if [ "$?" != "0" ]
 then
-    # Install rust and cargo
+    echo "Cargo is not installed. Installing...";
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
 
 which zkutil > /dev/null
 if [ "$?" != "0" ]
 then
-    # Install zkutil.
+    echo "zkutil is not installed. Installing...";
     cargo install zkutil --version 0.3.2 && zkutil --help
 fi
