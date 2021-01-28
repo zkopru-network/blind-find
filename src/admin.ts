4896ec324164e11764ef72bed761332410b96401
mhchia <kevin.mh.chia@gmail.com>
2021-01-28 15:38:46 +0800

    Add Admin
    Without tests, to be later used in CLIs

---
+++ src/admin.ts
@@ -0,0 +1,26 @@
+import { HubRegistry } from ".";
+import { HubRegistryTreeDB } from "./dataProvider";
+import { BlindFindContract } from "./web3";
+
+/**
+ * In charge of the merkle tree updating.
+ * NOTE: I'm thinking if it should inherit DataProvider.
+ */
+export class Admin {
+  constructor(
+    readonly contract: BlindFindContract,
+    readonly address: BigInt,
+    readonly treeDB: HubRegistryTreeDB
+  ) {}
+
+  private async updateMerkleRoot(root: BigInt) {
+    // NOTE: This function probably needs changing if admin is a multisig
+    await this.contract.updateMerkleRoot(root);
+  }
+
+  async insertHubRegistry(e: HubRegistry) {
+    await this.treeDB.insert(e);
+    const root = this.treeDB.tree.tree.root;
+    await this.updateMerkleRoot(root);
+  }
+}
