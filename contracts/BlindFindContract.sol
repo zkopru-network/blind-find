3d0533f7e2b91b5d2ec8947d8dcfe6b86e314e98
mhchia <kevin.mh.chia@gmail.com>
2021-01-29 16:37:23 +0800

    Remove console.log in contract

--- contracts/BlindFindContract.sol
+++ contracts/BlindFindContract.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.0;
 
-import "hardhat/console.sol";
-
 contract BlindFindContract {
 
   event UpdateMerkleRoot(
@@ -14,13 +12,11 @@ contract BlindFindContract {
 
   constructor() {
     admin = msg.sender;
-    console.log("BlindFindContract: is deployed by", admin);
   }
 
   function updateMerkleRoot(uint256 root) public {
     require (msg.sender == admin, "only admin can update the latest merkle root");
     latestMerkleRoot = root;
     emit UpdateMerkleRoot(latestMerkleRoot);
-    console.log("BlindFindContract: merkle root is updated to be", latestMerkleRoot);
   }
 }
