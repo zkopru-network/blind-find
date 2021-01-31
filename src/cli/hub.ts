7f07e5a83424638682cc3c3304f69cd087ff1867
mhchia <kevin.mh.chia@gmail.com>
2021-01-31 16:26:26 +0800

    `admin.addHub` print `hubRegistryWithProof`
    And `hubRegistryWithProof` is used as the input in
    `hub.setHubRegistryWithProof`.

--- src/cli/hub.ts
+++ src/cli/hub.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import { HubServer } from "../hub";
+import { HubServer, THubRegistryWithProof } from "../hub";
 import { LevelDB } from "../db";
 import { loadConfigs, parseHubConfig } from "./configs";
 import { dbDir } from "./defaults";
@@ -15,50 +15,69 @@ import {
 import { HubRegistry } from "..";
 import { ValueError } from "../exceptions";
 import { MerkleProof } from "../interfaces";
+import { BlindFindContract } from "../web3";
 
 export const buildCommandHub = () => {
   const command = new Command("hub");
   command
     .addCommand(buildCreateHubRegistry())
-    .addCommand(buildSetHubRegistry());
+    .addCommand(buildSetHubRegistryWithProof());
   return command;
 };
 
 const buildCreateHubRegistry = () => {
   const command = new Command("createHubRegistry");
-  command.description("create a hub registry").action(async () => {
-    const configs = await loadConfigs();
-    const networkConfig = configs.network;
-    const blindFindContract = getBlindFindContract(networkConfig);
-    const adminAddress = await blindFindContract.getAdmin();
-
-    const hubConfig = parseHubConfig(configs);
-    const hubKeypair: Keypair = {
-      privKey: hubConfig.blindFindPrivkey,
-      pubKey: genPubKey(hubConfig.blindFindPrivkey)
-    };
-
-    const hubRegistry = HubRegistry.fromKeypair(hubKeypair, adminAddress);
-    console.log(objToBase64(hubRegistryToObj(hubRegistry)));
-  });
-  return command;
-};
-
-const buildSetHubRegistry = () => {
-  const command = new Command("setHubRegistry");
   command
-    .arguments("<hubRegistry> <merkleProof>")
-    .description("set hub registry to the database", {
-      hubRegistry: "a HubRegistry object encoded in base64",
-      merkleProof: "the corresponding merkle proof encoded in base64"
-    })
-    .action(async (hubRegistryB64: string, merkleProofB64: string) => {
+    .description(
+      "Create a hub registry. To be registered as a valid hub, " +
+        "this registry must be set to admin and added to hub registry tree."
+    )
+    .action(async () => {
       const configs = await loadConfigs();
       const networkConfig = configs.network;
       const blindFindContract = getBlindFindContract(networkConfig);
       const adminAddress = await blindFindContract.getAdmin();
-      const hubRegistryObj = base64ToObj(hubRegistryB64);
-      const hubRegistry = objToHubRegistry(hubRegistryObj);
+
+      const hubConfig = parseHubConfig(configs);
+      const hubKeypair: Keypair = {
+        privKey: hubConfig.blindFindPrivkey,
+        pubKey: genPubKey(hubConfig.blindFindPrivkey)
+      };
+
+      const hubRegistry = HubRegistry.fromKeypair(hubKeypair, adminAddress);
+      console.log(objToBase64(hubRegistryToObj(hubRegistry)));
+    });
+  return command;
+};
+
+const buildSetHubRegistryWithProof = () => {
+  const command = new Command("setHubRegistryWithProof");
+  command
+    .arguments("<hubRegistryWithProof>")
+    .description(
+      "Set hub registry along with its merkle proof to the database",
+      {
+        hubRegistryWithProof:
+          "a `HubRegistryWithProof` object encoded in base64"
+      }
+    )
+    .action(async (hubRegistryWithProofB64: string) => {
+      const configs = await loadConfigs();
+      const networkConfig = configs.network;
+      const blindFindContract = getBlindFindContract(networkConfig);
+      const adminAddress = await blindFindContract.getAdmin();
+      const hubRegistryWithProofObj = base64ToObj(
+        hubRegistryWithProofB64
+      ) as THubRegistryWithProof;
+      if (
+        hubRegistryWithProofObj.hubRegistry === undefined ||
+        hubRegistryWithProofObj.merkleProof === undefined
+      ) {
+        throw new ValueError(
+          `hubRegistryWithProofObj does not have valid properties: hubRegistryWithProofObj=${hubRegistryWithProofObj}`
+        );
+      }
+      const hubRegistry = objToHubRegistry(hubRegistryWithProofObj.hubRegistry);
       // Verify hubRegistry
       if (hubRegistry.adminAddress !== adminAddress) {
         throw new ValueError(
@@ -69,46 +88,48 @@ const buildSetHubRegistry = () => {
       if (!hubRegistry.verify()) {
         throw new ValueError("hubRegistry has an invalid signature");
       }
-
       // Verify merkleProof
-      const merkleProof = base64ToObj(merkleProofB64) as MerkleProof;
-      if (merkleProof.leaf !== hubRegistry.hash()) {
-        throw new ValueError("merkleProof mismatches hubRegistry");
-      }
-      const hashFunc = (leaves: BigInt[]) => {
-        if (leaves.length !== 2) {
-          throw new Error(
-            `we're using binary merkle tree but got more than 2 elements: ` +
-              `length=${leaves.length}`
-          );
-        }
-        return hashLeftRight(leaves[0], leaves[1]);
-      };
-      if (!IncrementalQuinTree.verifyMerklePath(merkleProof, hashFunc)) {
-        throw new ValueError("merkleProof is invalid");
-      }
+      const merkleProof = hubRegistryWithProofObj.merkleProof;
+      validateMerkleProof(hubRegistry, merkleProof);
+      await validateMerkleRoot(blindFindContract, merkleProof.root);
 
+      // Store this valid hub registry and its proof.
       const levelDB = new LevelDB(dbDir);
-
-      await HubServer.setHubRegistryWithProof(levelDB, {
-        registry: hubRegistryToObj(hubRegistry),
+      await HubServer.setHubRegistryToDB(levelDB, {
+        hubRegistry: hubRegistryToObj(hubRegistry),
         merkleProof: merkleProof
       });
     });
-
   return command;
 };
 
-// const getHub = async () => {
-//   const configs = await loadConfigs();
-//   const networkConfig = configs.network;
-//   const hubConfig = parseHubConfig(configs);
-//   const hubKeypair: Keypair = {
-//       privKey: hubConfig.blindFindPrivkey,
-//       pubKey: genPubKey(hubConfig.blindFindPrivkey),
-//   };
-//   const blindFindContract = getBlindFindContract(networkConfig);
-//   const levelDB = new LevelDB(dbDir);
-//   await HubServer.setHubRegistryWithProof();
-//   const hub = new HubServer(hubKeypair, );
-// };
+const validateMerkleProof = (
+  hubRegistry: HubRegistry,
+  merkleProof: MerkleProof
+) => {
+  if (merkleProof.leaf !== hubRegistry.hash()) {
+    throw new ValueError("merkleProof mismatches hubRegistry");
+  }
+  const hashFunc = (leaves: BigInt[]) => {
+    if (leaves.length !== 2) {
+      throw new Error(
+        `we're using binary merkle tree but got more than 2 elements: ` +
+          `length=${leaves.length}`
+      );
+    }
+    return hashLeftRight(leaves[0], leaves[1]);
+  };
+  if (!IncrementalQuinTree.verifyMerklePath(merkleProof, hashFunc)) {
+    throw new ValueError("merkleProof is invalid");
+  }
+};
+
+const validateMerkleRoot = async (
+  contract: BlindFindContract,
+  merkleRoot: BigInt
+) => {
+  const allRoots = await contract.getAllMerkleRoots();
+  if (!allRoots.has(merkleRoot)) {
+    throw new ValueError("merkle root is not on chain");
+  }
+};
